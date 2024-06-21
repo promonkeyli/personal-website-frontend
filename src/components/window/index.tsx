c808d3b8f3a4f176209ce7f43780f51283cc49a2
young <liyang_email@163.com>
2024-06-22 01:08:15 +0800

    feat(components): add macos window component

--- src/components/window/index.tsx
+++ src/components/window/index.tsx
@@ -1,24 +1,27 @@
 import React from "react";
+import "./index.css";
 
 export interface IWindowProps {
   isOpen?: boolean;
 }
 export default function IWindow(props: IWindowProps) {
   return (
-    <div className="macos-window">
-      <div className="macos-title-bar">
-        <div className="macos-buttons">
-          <div className="macos-button red"></div>
-          <div className="macos-button yellow"></div>
-          <div className="macos-button green"></div>
-        </div>
-        <div className="text-lg font-medium text-gray-900">
-          macOS Style Window
-        </div>
-        <div></div>
+    <div className="w-[80vw] h-[80vh] bg-white mx-auto my-[20px] rounded-xl flex flex-col">
+      <div className="i-window-opt-container flex gap-2 p-2">
+        <span className="i-window-opt-icon bg-[#ed6b60]">
+          <span>-</span>
+        </span>
+        <span className="i-window-opt-icon bg-[#f4be4f]">
+          <span>+</span>
+        </span>
+        <span className="i-window-opt-icon bg-[#60c353]">
+          <span>x</span>
+        </span>
       </div>
-      <div className="macos-content">
-        <p>This is a macOS style window built with Tailwind CSS.</p>
+      <div className="flex-1 p-4 pt-2 grid place-content-center">
+        <span className="text-2xl text-[#b96ca0] font-semibold">
+          Mac OS Desktop Window Component
+        </span>
       </div>
     </div>
   );
